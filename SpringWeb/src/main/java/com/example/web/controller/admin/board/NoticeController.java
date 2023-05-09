3168ed138f2e5bd22cdce181567ccecce0e6876d
cleangg <cleangg123456>
2023-05-09 09:05:16 +0900

    asdf

---
+++ SpringWeb/src/main/java/com/example/web/controller/admin/board/NoticeController.java
@@ -0,0 +1,24 @@
+package com.example.web.controller.admin.board;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("adminNoticeController")
+@RequestMapping("/admin/board/notice/")
+public class NoticeController {
+	
+	@RequestMapping("list")
+	public String list() {
+		return "admin/board/notice/list";
+	}
+	@RequestMapping("detail")
+	public String detail() {
+		return "admin/board/notice/detail";
+	}
+	@RequestMapping("reg")
+	public String reg() {
+		return "admin/board/notice/reg";
+	}
+		
+}
---
+++ SpringWeb/src/main/java/com/example/web/controller/customer/NoticeController.java
@@ -0,0 +1,24 @@
+package com.example.web.controller.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NoticeController {
+
+	@RequestMapping("/customer/notice/list") // list,detail,edit,reg
+	public String list(Model model) {
+		model.addAttribute("test","hello~~" );
+		return "customer/notice/list";
+	}
+
+	@RequestMapping("/customer/notice/detail") // list,detail,edit,reg
+	public String detail() {
+
+		
+		
+		return "customer/notice/detail";
+	}
+
+}
